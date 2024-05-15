4c0e324f46f8c3352ebdfc78884aaef3e05758c9
Hamed Asghari <hamed@golestanfoundation.org>
2024-05-15 13:00:16 -0600

    Check if value is set in view

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -4,5 +4,5 @@
     {{-- Textarea --}}
     <textarea name="{{ $key }}" id="{{ $key }}" rows="{{ $rows ?? 2 }}" class="form-control"
         placeholder="{{ $placeholder ? (string) $placeholder : '' }}"
-        @if(isset($readonly) && $readonly) readonly @endif>{{ $value && $value instanceof \Illuminate\Support\Optional ? $value->value() : $value ?? null }}</textarea>
+        @if(isset($readonly) && $readonly) readonly @endif>{{ isset($value) && $value instanceof \Illuminate\Support\Optional ? $value->value() : $value ?? null }}</textarea>
 </div>
