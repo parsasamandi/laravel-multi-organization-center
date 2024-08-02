1671d7148fbdf8bf52da37312d7a6262ca7de414
Parsa Samandi <parsa_samandi@Pouyas-MacBook-Air.local>
2024-08-02 16:59:34 -0700

    Update the select box

--- resources/views/includes/jalaliYearsSelectBox.php
+++ resources/views/includes/jalaliYearsSelectBox.php
@@ -1,8 +1,8 @@
 <!-- HTML select box for Jalali years -->
 <label for="jalaliYear">سال: <span class="input-required">*</span></label>
 <select name="jalaliYear" id="jalaliYear">
-    <option value="1403">1403</option>
-    <option value="1404">1404</option>
-    <option value="1405">1405</option>
-    <option value="1406">1406</option>
+    <option value="1403">۱۴۰۳</option>
+    <option value="1404">۱۴۰۴</option>
+    <option value="1405">۱۴۰۵</option>
+    <option value="1406">۱۴۰۶</option>
 </select>
